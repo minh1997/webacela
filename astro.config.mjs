f557cb17b49cc9c3afb10857612ded254a5faaf1
Mink <congminh1203@gmail.com>
2025-12-16 20:19:45 +0700

    install sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://your-domain.com', // Replace with your actual domain
+  integrations: [tailwind(), sitemap()]
 });
